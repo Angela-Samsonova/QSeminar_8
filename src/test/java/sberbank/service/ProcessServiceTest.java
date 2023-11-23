84bdef37a640853201e497ea02cb7539bdceb2b7
an.samsonova <an.samsonova@sbereducation.ru>
2023-11-23 18:02:48 +0300

    Added new unit tests

--- src/test/java/sberbank/service/ProcessServiceTest.java
+++ src/test/java/sberbank/service/ProcessServiceTest.java
@@ -1,42 +1,76 @@
 package sberbank.service;
 
 import org.junit.jupiter.api.*;
+import ru.sberbank.data.Developer;
 import ru.sberbank.data.Task;
+import ru.sberbank.data.Tester;
 import ru.sberbank.service.*;
 
-import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.when;
+import java.util.ArrayList;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
 
 class ProcessServiceTest {
-
-    private ProcessService processService;
     private final int EXPECTED_TASK_ID = 0;
     private final String EXPECTED_TASK_SUMMARY = "Тестовая задача";
 
+    ProcessService processService;
+    TaskService taskService;
+    ReleaseService releaseService;
+    TesterService testerService;
+    DeveloperService developerService;
+
     @BeforeEach
-    void setUp() {
-        Task expectedTask = new Task(EXPECTED_TASK_ID, EXPECTED_TASK_SUMMARY);
+    void setup() {
+        Task developedAndTestedTask = new Task(1, "summary1");
+        developedAndTestedTask.setTested(true);
+        developedAndTestedTask.setDeveloped(true);
+        Task task = new Task(2, "summary2");
+        taskService = mock(TaskService.class);
+        when(taskService.getTask(1)).thenReturn(developedAndTestedTask);
+        when(taskService.getTask(2)).thenReturn(task);
 
-        TaskService taskService = mock(TaskService.class);
-        when(taskService.createTask(EXPECTED_TASK_ID, EXPECTED_TASK_SUMMARY)).thenReturn(expectedTask);
-
-        DeveloperService developerService = mock(DeveloperService.class);
-
-        TesterService testerService = mock(TesterService.class);
-
-        ReleaseService releaseService = mock(ReleaseService.class);
-
-        this.processService = new ProcessService(taskService, developerService, testerService, releaseService);
+        releaseService = mock(ReleaseService.class);
+        testerService = mock(TesterService.class);
+        developerService = mock(DeveloperService.class);
+        processService = new ProcessService(taskService, developerService, testerService, releaseService);
     }
 
     @Test
-    void createTask_successful(){
-        Assertions.assertEquals(0, processService.createTask(EXPECTED_TASK_SUMMARY));
+    void pushStatusTask_failed_exception_with_developed_and_tested_task() {
+        Exception exception = assertThrows(IllegalStateException.class, () -> {
+            processService.pushStatusTask(1);
+        });
+
+        assertEquals("Задача уже в финальном статусе!", exception.getMessage());
     }
 
     @Test
-    void pushStatusTask_successful(){
+    void pushStatusTask_failed_exception_when_developed_is_empty() {
+        Exception exception = assertThrows(IllegalStateException.class, () -> {
+            processService.pushStatusTask(2);
+        });
 
+        assertEquals("Нет свободных разработчиков!", exception.getMessage());
     }
 
+    @Test
+    void pushStatusTask_success() {
+        Developer freeDev = new Developer(1, "Konstantin", "Nesterov");
+        when(developerService.create(anyInt(), anyString(), anyString())).thenReturn(freeDev);
+        ArrayList<Developer> devs = new ArrayList<>();
+        devs.add(freeDev);
+        when(developerService.getListOfFree()).thenReturn(devs);
+        Tester freeTester = new Tester(1, "Andrey", "Kozin");
+        when(testerService.create(anyInt(), anyString(), anyString())).thenReturn(freeTester);
+        ArrayList<Tester> testers = new ArrayList<>();
+        testers.add(freeTester);
+        when(testerService.getListOfFree()).thenReturn(testers);
+
+        processService.pushStatusTask(2);
+
+        assertTrue(taskService.getTask(2).isDeveloped());
+        assertTrue(taskService.getTask(2).isTested());
+    }
 }
